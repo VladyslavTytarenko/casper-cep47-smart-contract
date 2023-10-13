2cd679b9b2cbfef8e21451ccb25cea401c261229
VladislavTitarenko <vladislav@1worldonline.com>
2023-10-13 14:22:30 +0300

    Modified cep47 smart contract

---
+++ cep47/src/event.rs
@@ -0,0 +1,32 @@
+use alloc::vec::Vec;
+use casper_types::Key;
+
+use crate::TokenId;
+
+pub enum CEP47Event {
+    Mint {
+        recipient: Key,
+        token_ids: Vec<TokenId>,
+    },
+    Burn {
+        owner: Key,
+        token_ids: Vec<TokenId>,
+    },
+    Approve {
+        owner: Key,
+        spender: Key,
+        token_ids: Vec<TokenId>,
+    },
+    Revoke {
+        owner: Key,
+        token_ids: Vec<TokenId>,
+    },
+    Transfer {
+        sender: Key,
+        recipient: Key,
+        token_ids: Vec<TokenId>,
+    },
+    MetadataUpdate {
+        token_id: TokenId,
+    },
+}
